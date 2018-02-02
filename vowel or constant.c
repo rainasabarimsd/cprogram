885833b63478fd195938fc33a490049bcd2da0e0
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-02-02 13:42:46 +0530

    Create vowel or constant.c

---
+++ vowel or constant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+printf("Enter the values");
+scanf("%c",&b);
+if(c=='a' !! c=='e' !! c=='i' !! c=='o' !! c=='u')
+printf("it is the vowel letter");
+else
+printf("it is not the vowel letter");
+getch();
+return 0;
+}
